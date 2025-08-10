e2710554a21e90df502e88cd80426e9420b762d1
vishnuiot <vishnuprasad.iit@gmail.com>
2025-08-10 11:20:08 +0200

    git commit -m "EC-43 5.2.3 Übergabe an eine konstate Referenz"

---
+++ Kapitel 5/5_2_funktion_referenz_const.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void ausgabegesamt(const int &anzahl, const double &preiseinzel)
+{
+    double preisgesamt;
+    preisgesamt = anzahl * preiseinzel;
+    cout << "gesamtpreis: " << preisgesamt << endl;
+}
+
+int main()
+{
+    int anzahl_apfel = 6, anzahl_birne = 10;
+    ausgabegesamt(anzahl_apfel, 5.50);
+    ausgabegesamt(anzahl_birne, 3.50);
+    ausgabegesamt(40, 5.6);
+    return 0;
+}
