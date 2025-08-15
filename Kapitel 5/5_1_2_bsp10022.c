09c56f9aeac497f2f80f2a02b800b2001ff2dec6
vishnuiot <vishnuprasad.iit@gmail.com>
2025-08-15 09:39:26 +0200

    git commit -m "WW007-33 5.1.2 Geschacteite Schleifen"

---
+++ Kapitel 5/5_1_2_bsp10022.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    double i,k,multiply;
+    for (i=1;i<10;i++)
+    {
+        for (k=1;k<10;k++)
+        {
+            multiply=i*k;
+            printf("%f x %f = %f \n",i,k,multiply);
+        }
+    }
+}
